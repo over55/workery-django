4b521e133f1dee7e9916b7836382aed6e39f7a01
bartmika <bart@mikasoftware.com>
2017-12-17 12:15:17 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # overfiftyfive-django
-The source code which powers the site ....
+**This library is currently being developed and is not production quality ready!**
+
+## Build Status
+TODO
+
+## Overview
+The web app engine which powers the web-application via ...
+
+## Minimum Requirements
+* Python 3.6
+
+
+## Installation
+1. Install the library.
+
+  ```bash
+  git clone https://github.com/over55/overfiftyfive-django.git
+  ```
+
+2. Create the database.
+
+  ```sql
+  drop database overfiftyfive_db;
+  create database overfiftyfive_db;
+  \c overfiftyfive_db;
+  CREATE USER django WITH PASSWORD '123password';
+  GRANT ALL PRIVILEGES ON DATABASE overfiftyfive_db to django;
+  ALTER USER django CREATEDB;
+  ALTER ROLE django SUPERUSER;
+  CREATE EXTENSION postgis;
+  ```
+3. Populate the environment variables for our project.
+
+  ```bash
+  ./setup_env_var.sh
+  ```
+
+4. You need to now modify the environment variables for your project...
+
+  ```
+  vi ./overfiftyfive/overfiftyfive/.env
+  ```
+
+5. Run in your console:
+
+  ```bash
+  python manage.py makemigrations; \
+  python manage.py migrate; \
+  python manage.py populate_public; \
+  python manage.py setup_fixtures;
+  ```
+
+6. Usage:
+
+  ```bash
+  sudo python manage.py runserver overfiftyfive.com:80
+  ```
+
+7. In your browser enter ``overfiftyfive.com:80``.
+
+
+## License
+This library is licensed under the **BSD** license. See [LICENSE](LICENSE) for more information.
---
+++ setup_env_var.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# setup.env.sh
+# The purpose of this script is to setup sample environment variables for this project. It is up to the responsibility of the developer to change these values once they are generated for production use.
+#
+
+# Step 1: Clear the file.
+clear;
+touch ./overfiftyfive/overfiftyfive/.env;
+echo '' > ./overfiftyfive/overfiftyfive/.env;
+
+# Setup 2: Populate the "Django" section.
+echo '#--------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo '# Django #' >> ./overfiftyfive/overfiftyfive/.env;
+echo '#--------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'SECRET_KEY=l7y)rwm2(@nye)rloo0=ugdxgqsywkiv&#20dqugj76w)s!!ns' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'IS_DEBUG=True' >> ./overfiftyfive/overfiftyfive/.env;
+echo "ALLOWED_HOSTS='*'" >> ./overfiftyfive/overfiftyfive/.env;
+echo "ADMIN_NAME='Bartlomiej Mika'" >> ./overfiftyfive/overfiftyfive/.env;
+echo "ADMIN_EMAIL=bart@mikasoftware.com" >> ./overfiftyfive/overfiftyfive/.env;
+
+# Step 3: Populate the "Database" section.
+echo '' >> ./overfiftyfive/overfiftyfive/.env;
+echo '#----------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo '# Database #' >> ./overfiftyfive/overfiftyfive/.env;
+echo '#----------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'SKIP' >> ./overfiftyfive/overfiftyfive/.env;
+echo '' >> ./overfiftyfive/overfiftyfive/.env;
+
+# Setp 4: Populate the "Email" section.
+echo '#-------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo '# Email #' >> ./overfiftyfive/overfiftyfive/.env;
+echo '#-------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'DEFAULT_TO_EMAIL=bart@mikasoftware.com' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'DEFAULT_FROM_EMAIL=postmaster@mover55london.ca' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'MAILGUN_ACCESS_KEY=<YOU_NEED_TO_PROVIDE>' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'MAILGUN_SERVER_NAME=over55london.ca' >> ./overfiftyfive/overfiftyfive/.env;
+echo '' >> ./overfiftyfive/overfiftyfive/.env;
+
+# Step X: Populate the "App variables" section.
+echo '#--------------------------------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo '# Application Specific Variables #' >> ./overfiftyfive/overfiftyfive/.env;
+echo '#--------------------------------#' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'O55_APP_HTTP_PROTOCOL=http://' >> ./overfiftyfive/overfiftyfive/.env;
+echo 'O55_APP_HTTP_DOMAIN=127.0.0.1:8080' >> ./overfiftyfive/overfiftyfive/.env;
