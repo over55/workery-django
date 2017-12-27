c2e1b31606e8d7628f4de891f836f181625cd5f8
bartmika <bart@mikasoftware.com>
2017-12-26 22:53:29 -0500

    Expanded unit tests.

---
+++ overfiftyfive/tenant_foundation/tests/test_utils.py
@@ -0,0 +1,43 @@
+from django.core.management import call_command
+from django_tenants.test.cases import TenantTestCase
+from django_tenants.test.client import TenantClient
+from django.urls import reverse
+from shared_foundation.models import SharedFranchise
+from tenant_foundation.utils import *
+
+
+
+class TestTenantUtils(TenantTestCase):
+    """
+    Console:
+    python manage.py test tenant_foundation.tests.test_utils
+    """
+
+    def setUp(self):
+        super(TestTenantUtils, self).setUp()
+        self.c = TenantClient(self.tenant)
+
+    def tearDown(self):
+        del self.c
+        super(TestTenantUtils, self).tearDown()
+
+    def test_bool_or_none(self):
+        result = bool_or_none("Bad")
+        self.assertFalse(result)
+
+        result = bool_or_none("1")
+        self.assertTrue(result)
+
+    def test_get_dt_from_toronto_timezone_ms_access_dt_string(self):
+        # Test a sample.
+        result = get_dt_from_toronto_timezone_ms_access_dt_string("1954-01-21, 12:00:00 AM")
+        self.assertIsNotNone(result)
+        self.assertIn("1954-01-21 12:00:00", str(result))
+
+        # Test with bad text.
+        result = get_dt_from_toronto_timezone_ms_access_dt_string("la la la")
+        self.assertIsNone(result)
+
+        # Test null.
+        result = get_dt_from_toronto_timezone_ms_access_dt_string(None)
+        self.assertIsNone(result)
