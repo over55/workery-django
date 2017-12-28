30c0aca999bb195d4009e0194b70a81befa9fca0
bartmika <bart@mikasoftware.com>
2017-12-27 19:07:09 -0500

    Added new API-endpoint.

--- overfiftyfive/shared_api/urls.py
+++ overfiftyfive/shared_api/urls.py
@@ -7,6 +7,7 @@ from shared_api.views.auth_logout_view import LogoutAPIView
 from shared_api.views.auth_send_reset_password_email_views import SendResetPasswordEmailAPIView
 from shared_api.views.auth_reset_password_views import ResetPasswordAPIView
 from shared_api.views.franchise_list_views import SharedFranchiseListAPIView
+from shared_api.views.get_is_unique_views import GetIsUniqueAPIView
 
 
 urlpatterns = [
@@ -19,6 +20,9 @@ urlpatterns = [
     url(r'^api/logout$', LogoutAPIView.as_view(), name='o55_logout_api_endpoint'),
     url(r'^api/reset-password$', ResetPasswordAPIView.as_view(), name='o55_reset_password_api_endpoint'),
     url(r'^api/send-reset-password-email$', SendResetPasswordEmailAPIView.as_view(), name='o55_send_reset_password_email_api_endpoint'),
+
+    # Functions.
+    url(r'^api/get_is_unique$', GetIsUniqueAPIView.as_view(), name='o55_get_is_unique_api_endpoint'),
 
     # Application.
     url(r'^api/franchises$', SharedFranchiseListAPIView.as_view(), name='o55_franchise_list_api_endpoint'),
---
+++ overfiftyfive/shared_api/views/get_is_unique_views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions, status
+from rest_framework.response import Response
+from shared_foundation.models import O55User
+
+
+class GetIsUniqueAPIView(APIView):
+    """
+    API-endpoint used for looking up an email address to check if is unique.
+    """
+    authentication_classes = (authentication.TokenAuthentication, )
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        email = self.request.query_params.get('email', '').lower()
+        is_email_unique = O55User.objects.filter(email=email).exists()
+        return Response(
+            data=1 if is_email_unique else 0,
+            status=status.HTTP_200_OK
+        )
