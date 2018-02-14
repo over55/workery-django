b61a40a44720fbffff452df4d7bf50fe1448109a
bartmika <bart@mikasoftware.com>
2018-02-13 23:22:44 -0500

    Initial commit for the Customer views.

--- overfiftyfive/tenant_customer/urls.py
+++ overfiftyfive/tenant_customer/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
+from django.urls import path
 from django.views.generic.base import RedirectView
 from tenant_customer.views import web_views
 
 
 urlpatterns = (
-    url(r'^customers$', web_views.master_page, name='o55_tenant_customer_master'),
-    url(r'^customer/(?P<pk>[^/.]+)/$', web_views.detail_page, name='o55_tenant_customer_detail'),
+    path('customers/', web_views.CustomerListView.as_view(), name='o55_tenant_customer_list'),
+    path('customers/<int:pk>/', web_views.CustomerDetailView.as_view(), name='o55_tenant_customer_detail'),
+
+    # url(r'^customer/create$', web_views.create_page, name='o55_tenant_customer_create'),
+    # url(r'^customer/(?P<pk>[^/.]+)/$', web_views.retrieve_or_update_page, name='o55_tenant_customer_retrieve_or_update'),
 )
--- overfiftyfive/tenant_customer/views/web_views.py
+++ overfiftyfive/tenant_customer/views/web_views.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from tenant_foundation.models import Customer
 
 
-@login_required(login_url="/login/")
-def master_page(request):
-    return render(request, 'tenant_customer/master_view.html',{
-        'current_page': 'customer-master',
-    })
+@method_decorator(login_required, name='dispatch')
+class CustomerListView(ListView):
+    context_object_name = 'customer_list'
+    queryset = Customer.objects.order_by('-created')
+    template_name = 'tenant_customer/customer_list.html'
+    paginate_by = 100
 
 
-@login_required(login_url="/login/")
-def detail_page(request, pk):
-    return render(request, 'tenant_customer/detail_view.html',{
-        'current_page': 'customer-master',
-    })
+@method_decorator(login_required, name='dispatch')
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = 'tenant_customer/customer_detail.html'
+
+    def get_object(self):
+        customer = super().get_object()  # Call the superclass
+        return customer  # Return the object
