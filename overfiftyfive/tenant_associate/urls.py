b62cec753e44b0b2b7fb40c6d783941e1bfece78
bartmika <bart@mikasoftware.com>
2018-02-17 15:26:41 -0500

    Reworked associates GUI to use similar GUI found in "InThough" templates.

--- overfiftyfive/tenant_associate/urls.py
+++ overfiftyfive/tenant_associate/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import include, url
+from django.urls import path
 from django.views.generic.base import RedirectView
 from tenant_associate.views import web_views
 
 
 urlpatterns = (
-    url(r'^associates$', web_views.master_page, name='o55_tenant_associate_master'),
-    url(r'^associates/(?P<pk>[^/.]+)/$', web_views.detail_page, name='o55_tenant_associate_detail'),
+    path('associates/', web_views.AssociateListView.as_view(), name='o55_tenant_associate_list'),
+    path('associate/create', web_views.AssociateCreateView.as_view(), name='o55_tenant_associate_create'),
+    path('associate/search', web_views.AssociateSearchView.as_view(), name='o55_tenant_associate_search'),
+    path('associate/<int:pk>/', web_views.AssociateDetailView.as_view(), name='o55_tenant_associate_detail'),
+
+    # url(r'^associate/create$', web_views.create_page, name='o55_tenant_associate_create'),
+    # url(r'^associate/(?P<pk>[^/.]+)/$', web_views.retrieve_or_update_page, name='o55_tenant_associate_retrieve_or_update'),
 )
--- overfiftyfive/tenant_associate/views/web_views.py
+++ overfiftyfive/tenant_associate/views/web_views.py
@@ -1,17 +1,61 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, FormView, UpdateView
+from django.views.generic import DetailView, ListView, TemplateView
+from django.utils.decorators import method_decorator
+from tenant_foundation.models import Associate
 
 
-@login_required(login_url="/login/")
-def master_page(request):
-    return render(request, 'tenant_associate/master_view.html',{
-        'current_page': 'associates',
-    })
+@method_decorator(login_required, name='dispatch')
+class AssociateListView(ListView):
+    context_object_name = 'associate_list'
+    queryset = Associate.objects.order_by('-created')
+    template_name = 'tenant_associate/associate_list.html'
+    paginate_by = 100
+
+    # def head(self, *args, **kwargs):
+    #     last_associate = self.get_queryset().latest('-last_modified')
+    #     response = HttpResponse('')
+    #     # RFC 1123 date format
+    #     response['Last-Modified'] = last_associate.last_modified.strftime('%a, %d %b %Y %H:%M:%S GMT')
+    #     return response
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "associates"
+        return context
 
 
-@login_required(login_url="/login/")
-def detail_page(request, pk):
-    return render(request, 'tenant_associate/detail_view.html',{
-        'current_page': 'associates',
-    })
+@method_decorator(login_required, name='dispatch')
+class AssociateSearchView(TemplateView):
+    template_name = 'tenant_associate/associate_search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "associates"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class AssociateCreateView(TemplateView):
+    template_name = 'tenant_associate/associate_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "associates"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class AssociateDetailView(DetailView):
+    model = Associate
+    template_name = 'tenant_associate/associate_detail.html'
+
+    def get_object(self):
+        associate = super().get_object()  # Call the superclass
+        return associate  # Return the object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "associates"
+        return context
