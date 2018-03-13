742436b3ab2777e7171678c53f8d2c647ae51b66
bartmika <bart@mikasoftware.com>
2018-03-12 22:38:42 -0400

    Initial commit for boilerplate code for the "tenant_order" app.

--- overfiftyfive/tenant_api/filters/order.py
+++ overfiftyfive/tenant_api/filters/order.py
@@ -1,2 +1,48 @@
 # -*- coding: utf-8 -*-
 import django_filters
+from phonenumber_field.modelfields import PhoneNumberField
+from tenant_foundation.models import Order
+from django.db import models
+
+
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = Order
+        fields = [
+            'customer',
+        ]
+        # filter_overrides = {
+        #     models.CharField: { # given_name
+        #         'filter_class': django_filters.CharFilter,
+        #         'extra': lambda f: {
+        #             'lookup_expr': 'icontains',
+        #         },
+        #     },
+        #     models.CharField: { # middle_name
+        #         'filter_class': django_filters.CharFilter,
+        #         'extra': lambda f: {
+        #             'lookup_expr': 'icontains',
+        #         },
+        #     },
+        #     models.CharField: { # last_name
+        #         'filter_class': django_filters.CharFilter,
+        #         'extra': lambda f: {
+        #             'lookup_expr': 'icontains',
+        #         },
+        #     },
+        #     models.CharField: { # owner__email
+        #         'filter_class': django_filters.CharFilter,
+        #         'extra': lambda f: {
+        #             'lookup_expr': 'icontains',
+        #         },
+        #     },
+        #     # DEVELOPERS NOTE:
+        #     # - We need custom overrides for the "django_filters" library to
+        #     #   work with the "django-phonenumber-field".
+        #     PhoneNumberField: {
+        #         'filter_class': django_filters.CharFilter,
+        #         'extra': lambda f: {
+        #             'lookup_expr': 'icontains',
+        #         },
+        #     }
+        # }
