592b5c402f9042172aa54d2364c7729538ce3ea8
bartmika <bart@mikasoftware.com>
2018-03-27 22:47:07 -0400

    Added placeholders to view.

--- overfiftyfive/tenant_order/urls.py
+++ overfiftyfive/tenant_order/urls.py
@@ -10,7 +10,14 @@ urlpatterns = (
     path('jobs/', web_views.JobSummaryView.as_view(), name='o55_tenant_job_summary'),
 
     # Create
-    path('jobs/create/step1/search-or-add', create_views.Step1CreateOrAddCustomerView.as_view(), name='o55_tenant_job_create'),
+    path('jobs/create/step-1/search-or-add', create_views.Step1CreateOrAddCustomerView.as_view(), name='o55_tenant_job_search_or_add_create'),
+    path('jobs/create/step-1/search-results', create_views.Step1CustomerSearchResultsView.as_view(), name='o55_tenant_job_customer_search_results_create'),
+    path('jobs/create/step-1/add-customer', create_views.Step1AddCustomerView.as_view(), name='o55_tenant_job_add_customer_create'),
+    path('jobs/create/step-1/add-customer-confirmation', create_views.Step1AddCustomerConfirmationView.as_view(), name='o55_tenant_job_add_customer_confirmation_create'),
+    path('jobs/create/step-2/', create_views.Step2View.as_view(), name='o55_tenant_job_step_2_create'),
+    path('jobs/create/step-3/', create_views.Step3View.as_view(), name='o55_tenant_job_step_3_create'),
+    path('jobs/create/step-4/', create_views.Step4View.as_view(), name='o55_tenant_job_step_4_create'),
+    path('jobs/create/step-5/', create_views.Step5View.as_view(), name='o55_tenant_job_step_5_create'),
 
     # List
     path('jobs/list/', web_views.JobListView.as_view(), name='o55_tenant_job_list'),
--- overfiftyfive/tenant_order/views/create_views.py
+++ overfiftyfive/tenant_order/views/create_views.py
@@ -9,10 +9,6 @@ from tenant_api.filters.order import OrderFilter
 from tenant_foundation.models.order import Order
 
 
-#--------#
-# CREATE #
-#--------#
-
 @method_decorator(login_required, name='dispatch')
 class Step1CreateOrAddCustomerView(TemplateView):
     template_name = 'tenant_order/create/step_1_search_or_add_view.html'
@@ -20,4 +16,74 @@ class Step1CreateOrAddCustomerView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['current_page'] = "jobs" # Required for navigation
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1CustomerSearchResultsView(TemplateView):
+    template_name = 'tenant_order/create/step_1_search_results_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1AddCustomerView(TemplateView):
+    template_name = 'tenant_order/create/step_1_create_customer_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1AddCustomerConfirmationView(TemplateView):
+    template_name = 'tenant_order/create/step_1_create_customer_confirmation_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step2View(TemplateView):
+    template_name = 'tenant_order/create/step_2_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step3View(TemplateView):
+    template_name = 'tenant_order/create/step_3_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step4View(TemplateView):
+    template_name = 'tenant_order/create/step_4_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step5View(TemplateView):
+    template_name = 'tenant_order/create/step_5_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
         return context
