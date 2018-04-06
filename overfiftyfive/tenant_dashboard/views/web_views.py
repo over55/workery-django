afb6bd3ecfc133000bd9c39e51d959ea26602201
bartmika <bart@mikasoftware.com>
2018-04-06 12:32:53 -0400

    Experiment with tenant_required decorator.

--- overfiftyfive/tenant_dashboard/views/web_views.py
+++ overfiftyfive/tenant_dashboard/views/web_views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from tenant_foundation.decorators import tenant_required
 from tenant_foundation.models.associate import Associate
 from tenant_foundation.models.customer import Customer
 from tenant_foundation.models.order import Order
 
 
 @login_required(login_url="login/")
+@tenant_required
 def master_page(request):
     """
     The default entry point into our dashboard.
---
+++ overfiftyfive/tenant_foundation/decorators.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseRedirect
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language
+
+
+def tenant_required(view_func):
+    """
+    Decorator ensures sub-domain is associated with the view request maps to
+    an existing (non-public) tenant or else return a forbidden error.
+    """
+    def wrapper(request, *args, **kwargs):
+        if not request.tenant.is_public:
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden(
+                _("Missing tenant.")
+            )
+    return wrapper
