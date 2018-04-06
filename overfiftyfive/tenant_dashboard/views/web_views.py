cca44d693912080e7648075b03ae17498ca3c0c6
bartmika <bart@mikasoftware.com>
2018-04-06 12:39:37 -0400

    Finished experiment

--- overfiftyfive/tenant_dashboard/views/web_views.py
+++ overfiftyfive/tenant_dashboard/views/web_views.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from tenant_foundation.decorators import tenant_required
+from tenant_foundation.decorators import tenant_required_or_404
 from tenant_foundation.models.associate import Associate
 from tenant_foundation.models.customer import Customer
 from tenant_foundation.models.order import Order
 
 
 @login_required(login_url="login/")
-@tenant_required
+@tenant_required_or_404
 def master_page(request):
     """
     The default entry point into our dashboard.
--- overfiftyfive/tenant_foundation/decorators.py
+++ overfiftyfive/tenant_foundation/decorators.py
@@ -3,20 +3,19 @@ from functools import wraps
 from django.conf import settings
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseRedirect
+from django.http import Http404
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import get_language
 
 
-def tenant_required(view_func):
+def tenant_required_or_404(view_func):
     """
     Decorator ensures sub-domain is associated with the view request maps to
-    an existing (non-public) tenant or else return a forbidden error.
+    an existing (non-public) tenant or else return a 404 error.
     """
     def wrapper(request, *args, **kwargs):
         if not request.tenant.is_public():
             return view_func(request, *args, **kwargs)
         else:
-            return HttpResponseForbidden(
-                _("Missing tenant.")
-            )
+            raise Http404()
     return wrapper
