b975ff18f6a54702a0351037e0fb4ea3bb552b51
bartmika <bart@mikasoftware.com>
2018-04-12 15:56:13 -0400

    Added biz/rezl users in job creation screen.

--- overfiftyfive/shared_api/views/get_is_unique_views.py
+++ overfiftyfive/shared_api/views/get_is_unique_views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
+from django.db import connection # Used for django tenants.
 from rest_framework.views import APIView
 from rest_framework import authentication, permissions, status
 from rest_framework.response import Response
@@ -13,8 +14,11 @@ class GetIsUniqueAPIView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
+        connection.set_schema_to_public() # Switch to Public.
+
         email = self.request.POST.get('email', '').lower()
-        found_email = SharedUser.objects.filter(email=email).exists()
+        found_email = SharedUser.objects.filter(email__iexact=email).exists()
+
         return Response(
             data=0 if found_email else 1,
             status=status.HTTP_200_OK
--- overfiftyfive/tenant_order/views/create_views.py
+++ overfiftyfive/tenant_order/views/create_views.py
@@ -7,12 +7,12 @@ from django.utils.translation import ugettext_lazy as _
 from shared_foundation.mixins import ExtraRequestProcessingMixin
 from tenant_api.filters.order import OrderFilter
 from tenant_api.filters.customer import CustomerFilter
-from tenant_foundation.models import Customer, Order, SkillSet
+from tenant_foundation.models import Customer, Order, SkillSet, Tag
 
 
 @method_decorator(login_required, name='dispatch')
-class Step1CreateOrAddCustomerView(TemplateView):
-    template_name = 'tenant_order/create/step_1_search_or_add_view.html'
+class Step1A1CreateOrAddCustomerView(TemplateView):
+    template_name = 'tenant_order/create/step_1_a_1_search_or_add_view.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,10 +21,10 @@ class Step1CreateOrAddCustomerView(TemplateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class Step1CustomerSearchResultsView(ListView, ExtraRequestProcessingMixin):
+class Step1A2CustomerSearchResultsView(ListView, ExtraRequestProcessingMixin):
     context_object_name = 'customer_list'
     queryset = Customer.objects.order_by('-created')
-    template_name = 'tenant_order/create/step_1_search_results_view.html'
+    template_name = 'tenant_order/create/step_1_a_2_search_results_view.html'
     paginate_by = 100
 
     def get_context_data(self, **kwargs):
@@ -54,7 +54,7 @@ class Step1CustomerSearchResultsView(ListView, ExtraRequestProcessingMixin):
             queryset = Customer.objects.full_text_search(keyword)
             queryset = queryset.order_by('-created')
         else:
-            queryset = super(Step1CustomerSearchResultsView, self).get_queryset()
+            queryset = super(Step1A2CustomerSearchResultsView, self).get_queryset()
             filter = CustomerFilter(self.request.GET, queryset=queryset)
             queryset = filter.qs
 
@@ -62,7 +62,17 @@ class Step1CustomerSearchResultsView(ListView, ExtraRequestProcessingMixin):
 
 
 @method_decorator(login_required, name='dispatch')
-class Step1AddCustomerView(TemplateView):
+class Step1B1PickCustomerView(TemplateView):
+    template_name = 'tenant_order/create/step_1_b_1_pick_customer_type_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs" # Required for navigation
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1B1AddResidentialCustomerView(TemplateView):
     template_name = 'tenant_order/create/step_1_create_customer_view.html'
 
     def get_context_data(self, **kwargs):
@@ -71,9 +81,44 @@ class Step1AddCustomerView(TemplateView):
         return context
 
 
+
+
 @method_decorator(login_required, name='dispatch')
-class Step1AddCustomerConfirmationView(TemplateView):
-    template_name = 'tenant_order/create/step_1_create_customer_confirmation_view.html'
+class Step1B2AAddResidentialCustomerView(TemplateView):
+    template_name = 'tenant_order/create/step_1_b_2_a_reisdential_create_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1B2BAddCustomerConfirmationView(TemplateView):
+    template_name = 'tenant_order/create/step_1_b_2_b_reisdential_confirm_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1B3AAddCommercialCustomerView(TemplateView):
+    template_name = 'tenant_order/create/step_1_b_3_a_commercial_create_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = "jobs"
+        context['current_page'] = "jobs"
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class Step1B3BAddCommercialConfirmationView(TemplateView):
+    template_name = 'tenant_order/create/step_1_b_3_b_commercial_confirm_view.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
