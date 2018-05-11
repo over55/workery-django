ac32ba2817fde791022dc59694f18e68e6214eb1
bartmika <bart@mikasoftware.com>
2018-05-11 14:05:21 -0400

    Updated lite/full retrieve pages. #159

--- overfiftyfive/tenant_customer/urls.py
+++ overfiftyfive/tenant_customer/urls.py
@@ -23,8 +23,9 @@ urlpatterns = (
     path('clients/search/results/', search_view.CustomerSearchResultView.as_view(), name='o55_tenant_customer_search_results'),
 
     # Retrieve
-    path('clients/<str:template>/detail/<int:pk>/', retrieve_view.CustomerRetrieveView.as_view(), name='o55_tenant_customer_retrieve'),
-    path('clients/<str:template>/details/comments/<int:pk>/', retrieve_view.CustomerCommentRetrieveView.as_view(), name='o55_tenant_customer_comments_retrieve'),
+    path('clients/<str:template>/detail/<int:pk>/lite/', retrieve_view.CustomerLiteRetrieveView.as_view(), name='o55_tenant_customer_lite_retrieve'),
+    path('clients/<str:template>/detail/<int:pk>/full/', retrieve_view.CustomerFullRetrieveView.as_view(), name='o55_tenant_customer_full_retrieve'),
+    path('clients/<str:template>/detail/<int:pk>/comments/', retrieve_view.CustomerCommentRetrieveView.as_view(), name='o55_tenant_customer_comments_retrieve'),
 
     # Update
     path('clients/<str:template>/detail/<int:pk>/edit/', update_view.CustomerUpdateView.as_view(), name='o55_tenant_customer_update'),
