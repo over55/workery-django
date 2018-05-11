812cd550c02f1134587f121f77b6d7acbc619045
bartmika <bart@mikasoftware.com>
2018-05-11 13:48:04 -0400

    Updated a jobs list for the associate page. #157

--- overfiftyfive/tenant_associate/urls.py
+++ overfiftyfive/tenant_associate/urls.py
@@ -20,10 +20,11 @@ urlpatterns = (
     path('members/search/results/', search_view.MemberSearchResultView.as_view(), name='o55_tenant_member_search_results'),
 
     # Retrieve
-    path('members/<str:template>/lite-detail/<int:pk>/', retrieve_view.MemberLiteRetrieveView.as_view(), name='o55_tenant_member_lite_retrieve'),
-    path('members/<str:template>/full-detail/<int:pk>/', retrieve_view.MemberFullRetrieveView.as_view(), name='o55_tenant_member_full_retrieve'),
-    path('members/<str:template>/comments/<int:pk>/', retrieve_view.MemberRetrieveForCommentsListAndCreateView.as_view(), name='o55_tenant_member_retrieve_for_comment_list'),
-    path('members/<str:template>/activity-sheets/<int:pk>/', retrieve_view.MemberRetrieveForActivitySheetListView.as_view(), name='o55_tenant_member_retrieve_for_activity_sheet_list'),
+    path('members/<str:template>/detail/<int:pk>/lite/', retrieve_view.MemberLiteRetrieveView.as_view(), name='o55_tenant_member_lite_retrieve'),
+    path('members/<str:template>/detail/<int:pk>/full/', retrieve_view.MemberFullRetrieveView.as_view(), name='o55_tenant_member_full_retrieve'),
+    path('members/<str:template>/detail/<int:pk>/comments/', retrieve_view.MemberRetrieveForCommentsListAndCreateView.as_view(), name='o55_tenant_member_retrieve_for_comment_list'),
+    path('members/<str:template>/detail/<int:pk>/activity-sheets/', retrieve_view.MemberRetrieveForActivitySheetListView.as_view(), name='o55_tenant_member_retrieve_for_activity_sheet_list'),
+    path('members/<str:template>/detail/<int:pk>/jobs/', retrieve_view.MemberRetrieveForJobsListView.as_view(), name='o55_tenant_member_retrieve_for_jobs_list'),
 
     # Update
     path('members/<str:template>/detail/<int:pk>/edit/', update_view.MemberUpdateView.as_view(), name='o55_tenant_member_update'),
--- overfiftyfive/tenant_associate/views/retrieve_view.py
+++ overfiftyfive/tenant_associate/views/retrieve_view.py
@@ -143,10 +143,43 @@ class MemberRetrieveForActivitySheetListView(DetailView, ExtraRequestProcessingM
         #   so we can use this to help the pagination.
         modified_context['parameters'] = self.get_params_dict([])
 
-        # #
+        # Lookup the acitivty sheet items for this associate.
         modified_context['activity_sheet_items'] = ActivitySheetItem.objects.filter(
             associate = modified_context['associate']
         ).order_by("-created_at")
+
+        # Return our modified context.
+        return modified_context
+
+
+@method_decorator(login_required, name='dispatch')
+class MemberRetrieveForJobsListView(DetailView, ExtraRequestProcessingMixin):
+    context_object_name = 'associate'
+    model = Associate
+    template_name = 'tenant_associate/retrieve/for/jobs_view.html'
+
+    def get_object(self):
+        associate = super().get_object()  # Call the superclass
+        return associate                  # Return the object
+
+    def get_context_data(self, **kwargs):
+        # Get the context of this class based view.
+        modified_context = super().get_context_data(**kwargs)
+
+        # Validate the template selected.
+        template = self.kwargs['template']
+        if template not in ['search', 'summary', 'list']:
+            from django.core.exceptions import PermissionDenied
+            raise PermissionDenied(_('You entered wrong format.'))
+        modified_context['template'] = template
+
+        # Required for navigation
+        modified_context['current_page'] = "associates"
+
+        # DEVELOPERS NOTE:
+        # - We will extract the URL parameters and save them into our context
+        #   so we can use this to help the pagination.
+        modified_context['parameters'] = self.get_params_dict([])
 
         # Return our modified context.
         return modified_context
