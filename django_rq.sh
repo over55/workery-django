4cfd852f9a96586745da06641c4489080e8216d8
bartmika <bart@mikasoftware.com>
2018-05-11 23:13:06 -0400

    Refactored "home" to "opt" for production stuff.

--- django_rq.sh
+++ django_rq.sh
@@ -5,14 +5,14 @@
 # script:
 
 # (1) This project must be located in the following directory:
-#     "/home/django/overfiftyfive-django"
+#     "/opt/django/overfiftyfive-django"
 # (2) The "env" variable has already been initialized with the projects
 #     requirements.txt file.
 # (3) The operating system is "CentOS 7".
 # (4) There is a "django" user account created on the "CentOS 7" system.
 #
 
-cd /home/django/overfiftyfive-django
+cd /opt/django/overfiftyfive-django
 source env/bin/activate
-cd /home/django/overfiftyfive-django/overfiftyfive
+cd /opt/django/overfiftyfive-django/overfiftyfive
 exec python manage.py rqworker
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/home/django/overfiftyfive-django/env/bin/gunicorn'
-pythonpath = '/home/django/overfiftyfive-django/overfiftyfive'
+command = '/opt/django/overfiftyfive-django/env/bin/gunicorn'
+pythonpath = '/opt/django/overfiftyfive-django/overfiftyfive'
 bind = '127.0.0.1:8001'
 workers = 3
