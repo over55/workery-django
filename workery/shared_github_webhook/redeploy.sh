4fa475f240ecc9d2c3e23ca6a9b7e3c1b3eee918
bartmika <bart@mikasoftware.com>
2018-05-19 01:51:33 -0400

    Added another comment

--- workery/shared_github_webhook/redeploy.sh
+++ workery/shared_github_webhook/redeploy.sh
@@ -7,3 +7,8 @@ echo "I am...";
 whoami;
 echo "Resarting..."
 sudo systemctl restart supervisord;
+
+# https://stackoverflow.com/a/24107529
+# username ALL = NOPASSWD: /opt/django/workery-django/workery/shared_github_webhook/redeploy.sh
+
+echo "Finished"
