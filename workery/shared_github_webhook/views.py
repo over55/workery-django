8bcd5ea40e91b8dce3933b753bde32db70f26a17
bartmika <bart@mikasoftware.com>
2018-05-19 01:08:10 -0400

    Added a github handler

--- workery/shared_github_webhook/views.py
+++ workery/shared_github_webhook/views.py
@@ -16,5 +16,42 @@ from django.views.decorators.csrf import csrf_exempt
 @require_POST
 @csrf_exempt
 def github_webhook_handler(request):
-    print("######## github_webhook_handler #######")
-    return HttpResponse('pong')
+    """
+    https://gist.github.com/vitorfs/145a8b8f0865cb65ee915e0c846fc303
+    """
+    # Verify if request came from GitHub
+    # forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
+    # client_ip_address = ip_address(forwarded_for)
+    # whitelist = requests.get('https://api.github.com/meta').json()['hooks']
+
+    # for valid_ip in whitelist:
+    #     if client_ip_address in ip_network(valid_ip):
+    #         break
+    # else:
+    #     return HttpResponseForbidden('Permission denied.')
+
+    # Verify the request signature
+    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    if header_signature is None:
+        return HttpResponseForbidden('Permission denied.')
+
+    sha_name, signature = header_signature.split('=')
+    if sha_name != 'sha1':
+        return HttpResponseServerError('Operation not supported.', status=501)
+
+    mac = hmac.new(force_bytes(settings.O55_GITHUB_WEBHOOK_SECRET), msg=force_bytes(request.body), digestmod=sha1)
+    if not hmac.compare_digest(force_bytes(mac.hexdigest()), force_bytes(signature)):
+        return HttpResponseForbidden('Permission denied.')
+
+    # If request reached this point we are in a good shape
+    # Process the GitHub events
+    event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+
+    if event == 'ping':
+        return HttpResponse('pong')
+    elif event == 'push':
+        # Deploy some code for example
+        return HttpResponse('success')
+
+    # In case we receive an event that's not ping or push
+    return HttpResponse(status=204)
