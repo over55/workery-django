9d6285953a38f10b79978da8607e6c8979697905
bartmika <bart@mikasoftware.com>
2018-05-20 13:57:48 -0400

    Updating redeployment script.

--- workery/shared_github_webhook/redeploy.sh
+++ workery/shared_github_webhook/redeploy.sh
@@ -12,6 +12,9 @@ echo "Updating database...";
 cd workery
 python manage.py migrate
 
+echo "Collecting static files...";
+python manage.py collectstatic --noinput;
+
 echo "Resarting supervisord..."
 systemctl restart supervisord;
 
