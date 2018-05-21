a01644c8786e73947c2650e7edc3bc947ec0532f
bartmika <bart@mikasoftware.com>
2018-05-21 01:38:12 -0400

    First commit for reports generation and downloading. Prototype code. #46

--- workery/tenant_report/urls.py
+++ workery/tenant_report/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import include, url
 from django.views.generic.base import RedirectView
-from tenant_report.views import ReportListView
+from tenant_report.views import (
+    ReportListView,
+    Report09DetailView,
+    some_streaming_csv_view
+)
 
 
 urlpatterns = (
     url(r'^reports$', ReportListView.as_view(), name='workery_tenant_reports_list_master'),
+    url(r'^report/9/$', Report09DetailView.as_view(), name='workery_tenant_report_09_detail_master'),
+
+    url(r'^report/9/download$', some_streaming_csv_view, name='workery_tenant_report_09_download_file'),
 )
--- workery/tenant_report/views.py
+++ workery/tenant_report/views.py
@@ -17,13 +17,50 @@ from tenant_foundation.models import (
 
 @method_decorator(login_required, name='dispatch')
 class ReportListView(TemplateView, ExtraRequestProcessingMixin):
-    """
-    The default entry point into our dashboard.
-    """
-
     template_name = 'tenant_report/list_view.html'
 
     def get_context_data(self, **kwargs):
         modified_context = super().get_context_data(**kwargs)
         modified_context['current_page'] = 'reports' # Required
         return modified_context
+
+
+@method_decorator(login_required, name='dispatch')
+class Report09DetailView(TemplateView, ExtraRequestProcessingMixin):
+    template_name = 'tenant_report/report_09_view.html'
+
+    def get_context_data(self, **kwargs):
+        modified_context = super().get_context_data(**kwargs)
+        modified_context['current_page'] = 'reports' # Required
+        return modified_context
+
+
+"""
+Code below was taken from:
+https://docs.djangoproject.com/en/2.0/howto/outputting-csv/
+"""
+
+import csv
+
+from django.http import StreamingHttpResponse
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+def some_streaming_csv_view(request):
+    """A view that streams a large CSV file."""
+    # Generate a sequence of rows. The range is based on the maximum number of
+    # rows that can be handled by a single sheet in most spreadsheet
+    # applications.
+    rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse((writer.writerow(row) for row in rows),
+                                     content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+    return response
