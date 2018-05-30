390f94f1f25efb3db95789284bf69f133dc2b8a2
bartmika <bart@mikasoftware.com>
2018-05-30 19:33:10 -0400

    Added minor change.

--- workery/workery/settings.py
+++ workery/workery/settings.py
@@ -356,8 +356,16 @@ TEMPLATE_DIRS = (
 # https://github.com/ottoyiu/django-cors-headers
 
 CORS_ORIGIN_ALLOW_ALL=True
-CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
-                      'content-type', 'accept', 'origin', 'authorization')
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-disposition',
+    'accept-encoding',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken'
+)
 
 
 # django-debug-toolbar
