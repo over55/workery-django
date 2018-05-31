b9d0efeab4c42230bf06fb4651406c91a9db9cb4
bartmika <bart@mikasoftware.com>
2018-05-30 22:33:50 -0400

    Updated the model schema.

--- workery/shared_foundation/apps.py
+++ workery/shared_foundation/apps.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
+import django_rq
+from rq_scheduler import Scheduler
+from datetime import datetime, timedelta
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.management import call_command
 
 
 class SharedFoundationConfig(AppConfig):
     name = 'shared_foundation'
+
+    def ready(self):
+        scheduler = django_rq.get_scheduler('default')
+
+        for job in scheduler.get_jobs():
+            if "shared_foundation" in str(job):
+                job.delete()
