2f7b773482a70410d7a60a177bb0397aa6889c2f
bartmika <bart@mikasoftware.com>
2018-07-07 23:07:59 -0400

    Added initial commit for the ongoing job details page. #363

--- workery/tenant_ongoing_order/urls.py
+++ workery/tenant_ongoing_order/urls.py
@@ -6,9 +6,11 @@ from tenant_ongoing_order import views
 urlpatterns = (
     # List
     path('ongoing-jobs/list-summary/', views.OngoingJobListView.as_view(), name='workery_tenant_ongoing_job_list'),
-    # path('ongoing-jobs/list-all/', views.AllOngoingJobListView.as_view(), name='workery_tenant_all_ongoing_job_list'),
 
+    # Retrieve
     path('ongoing-jobs/<str:template>/detail/<int:pk>/lite/', views.OngoingJobLiteRetrieveView.as_view(), name='workery_tenant_ongoing_job_lite_retrieve'),
     path('ongoing-jobs/<str:template>/detail/<int:pk>/full/', views.OngoingJobFullRetrieveView.as_view(), name='workery_tenant_ongoing_job_full_retrieve'),
 
+    # Update
+    path('ongoing-jobs/<str:template>/detail/<int:pk>/edit/', views.OngoingJobUpdateView.as_view(), name='workery_tenant_ongoing_job_update'),
 )
--- workery/tenant_ongoing_order/views.py
+++ workery/tenant_ongoing_order/views.py
@@ -74,3 +74,27 @@ class OngoingJobFullRetrieveView(LoginRequiredMixin, WorkeryDetailView):
 
         # Return our modified context.
         return modified_context
+
+
+class OngoingJobUpdateView(LoginRequiredMixin, WorkeryDetailView):
+    context_object_name = 'job'
+    model = OngoingWorkOrder
+    template_name = 'tenant_order/update/view.html'
+    menu_id = 'jobs'
+
+    def get_context_data(self, **kwargs):
+        # Get the context of this class based view.
+        modified_context = super().get_context_data(**kwargs)
+
+        # Validate the template selected.
+        template = self.kwargs['template']
+        if template not in ['search', 'summary', 'list']:
+            from django.core.exceptions import PermissionDenied
+            raise PermissionDenied(_('You entered wrong format.'))
+        modified_context['template'] = template
+
+        # Set our dependencies
+        modified_context['skillsets'] = SkillSet.objects.all()
+
+        # Return our modified context.
+        return modified_context
