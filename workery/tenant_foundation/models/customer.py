822b3da6768e3604876846a9610373d6eaaa7efa
bartmika <bart@mikasoftware.com>
2018-07-22 22:05:40 -0400

    Changed the database model field from `CharField` to `IntegerField`. #422

--- workery/tenant_foundation/models/customer.py
+++ workery/tenant_foundation/models/customer.py
@@ -144,27 +144,6 @@ class Customer(AbstractPerson):
         blank=True,
         null=True,
     )
-    how_hear = models.CharField(
-        _("Learned about Over 55"),
-        max_length=2055,
-        help_text=_('How customer heared/learned about this Over 55 Inc.'),
-        blank=True,
-        null=True,
-    )
-    # how_hear = models.PositiveSmallIntegerField(
-    #     _("Learned about us"),
-    #     help_text=_('How customer heared/learned about this Over 55 Inc.'),
-    #     blank=True,
-    #     null=True,
-    # )
-    how_hear_other = models.CharField(
-        _("Learned about us (other)"),
-        max_length=2055,
-        help_text=_('How customer heared/learned about this Over 55 Inc.'),
-        blank=True,
-        null=True,
-    )
-    """
     how_hear = models.PositiveSmallIntegerField(
         _("Learned about us"),
         help_text=_('How customer heared/learned about this Over 55 Inc.'),
@@ -178,7 +157,6 @@ class Customer(AbstractPerson):
         blank=True,
         default="Did not answer"
     )
-    """
     created_by = models.ForeignKey(
         SharedUser,
         help_text=_('The user whom created this object.'),
