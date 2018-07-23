d990b763f034b451a3b1110fdc97fc44e5492cb2
bartmika <bart@mikasoftware.com>
2018-07-22 21:30:32 -0400

    Expanded model to support how heard. Added data migration ETL. #422

--- workery/tenant_foundation/models/customer.py
+++ workery/tenant_foundation/models/customer.py
@@ -157,13 +157,13 @@ class Customer(AbstractPerson):
     #     blank=True,
     #     null=True,
     # )
-    # how_hear_other = models.CharField(
-    #     _("Learned about us (other)"),
-    #     max_length=2055,
-    #     help_text=_('How customer heared/learned about this Over 55 Inc.'),
-    #     blank=True,
-    #     null=True,
-    # )
+    how_hear_other = models.CharField(
+        _("Learned about us (other)"),
+        max_length=2055,
+        help_text=_('How customer heared/learned about this Over 55 Inc.'),
+        blank=True,
+        null=True,
+    )
     created_by = models.ForeignKey(
         SharedUser,
         help_text=_('The user whom created this object.'),
