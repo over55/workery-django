0778741f8a10f77f093899095bf243c6dad66f69
bartmika <bart@mikasoftware.com>
2018-08-21 22:50:52 -0400

    BUGFIX: Added ETL to fix score. #508

---
+++ workery/tenant_foundation/management/commands/hotfix_10.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection # Used for django tenants.
+from django.db.models import Q, Prefetch
+from django.utils.translation import ugettext_lazy as _
+from starterkit.utils import (
+    get_random_string,
+    get_unique_username_from_email
+)
+from shared_foundation.constants import *
+from shared_foundation.models import (
+    SharedUser,
+    SharedFranchise
+)
+from tenant_foundation.constants import *
+from tenant_foundation.models import (
+    Associate,
+    # Comment,
+    Customer,
+    InsuranceRequirement,
+    Organization,
+    WORK_ORDER_STATE,
+    WorkOrder,
+    # WorkOrderComment,
+    WorkOrderServiceFee,
+    ResourceCategory,
+    ResourceItem,
+    ResourceItemSortOrder,
+    SkillSet,
+    Staff,
+    Tag,
+    VehicleType,
+    ONGOING_WORK_ORDER_STATE,
+    OngoingWorkOrder,
+    TaskItem
+)
+from tenant_foundation.utils import *
+
+
+class Command(BaseCommand):
+    help = _('Command will run `hotfix_10`.')
+
+    def add_arguments(self, parser):
+        """
+        Run manually in console:
+        python manage.py hotfix_10 "london"
+        """
+        parser.add_argument('schema_name', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        # Connection needs first to be at the public schema, as this is where
+        # the database needs to be set before creating a new tenant. If this is
+        # not done then django-tenants will raise a "Can't create tenant outside
+        # the public schema." error.
+        connection.set_schema_to_public() # Switch to Public.
+        # Get the user inputs.
+        schema_name = options['schema_name'][0]
+
+        try:
+            franchise = SharedFranchise.objects.get(schema_name=schema_name)
+        except SharedFranchise.DoesNotExist:
+            raise CommandError(_('Franchise does not exist!'))
+
+        # Connection will set it back to our tenant.
+        connection.set_schema(franchise.schema_name, True) # Switch to Tenant.
+
+        for job in WorkOrder.objects.all():
+            score = 0
+            if job.was_survey_conducted:
+                score += 1
+            if job.was_job_satisfactory:
+                score += 1
+            if job.was_job_finished_on_time_and_on_budget:
+                score += 1
+            if job.was_associate_punctual:
+                score += 1
+            if job.was_associate_professional:
+                score += 1
+            if job.would_customer_refer_our_organization:
+                score += 1
+            job.score = score
+            job.save()
+            
+            self.stdout.write(
+                self.style.SUCCESS(_('Updated job # %(id)s.') % { 'id': str(job.id) })
+            )
+
+        # For debugging purposes.
+        self.stdout.write(
+            self.style.SUCCESS(_('Successfully updated.'))
+        )
