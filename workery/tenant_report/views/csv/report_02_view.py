d45973ebab39229faea6e2121544e74f6410cc90
bartmika <bart@mikasoftware.com>
2018-08-23 23:11:46 -0400

    Removed one field. #511

--- workery/tenant_report/views/csv/report_02_view.py
+++ workery/tenant_report/views/csv/report_02_view.py
@@ -107,7 +107,6 @@ def report_02_streaming_csv_view(request):
         "Client No.",
         "Client Name",
         "Skill Set(s)",
-        "Was survey conducted",
         "Was job_ satisfactory",
         "Was job finished on time and on budget",
         "Was associate punctual",
@@ -133,8 +132,7 @@ def report_02_streaming_csv_view(request):
         invoice_service_fee_amount = str(job.invoice_service_fee_amount)
         invoice_service_fee_amount = invoice_service_fee_amount.replace('C', '')
 
-        #
-        was_survey_conducted = '-'
+        # Set to empty.
         was_job_satisfactory = '-'
         was_job_finished_on_time_and_on_budget = '-'
         was_associate_punctual = '-'
@@ -143,7 +141,6 @@ def report_02_streaming_csv_view(request):
         score = '-'
 
         if job.state == WORK_ORDER_STATE.COMPLETED_AND_PAID or job.state == WORK_ORDER_STATE.COMPLETED_BUT_UNPAID:
-            was_survey_conducted = 1 if job.was_survey_conducted else 0
             was_job_satisfactory = 1 if job.was_job_satisfactory else 0
             was_job_finished_on_time_and_on_budget = 1 if job.was_job_finished_on_time_and_on_budget else 0
             was_associate_punctual = 1 if job.was_associate_punctual else 0
@@ -163,7 +160,6 @@ def report_02_streaming_csv_view(request):
             job.customer.id,
             str(job.customer),
             skill_set_string,
-            was_survey_conducted,
             was_job_satisfactory,
             was_job_finished_on_time_and_on_budget,
             was_associate_punctual,
