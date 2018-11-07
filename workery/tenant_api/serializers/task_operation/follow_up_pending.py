ec9969540004ff7a898d6a4da5aaf3ede3b90478
rodolfomartinez <rodolfo@theshootingstarpress.com>
2018-11-07 09:53:35 -0500

    Text change
    change to reflect updated processes

--- workery/tenant_api/serializers/task_operation/follow_up_pending.py
+++ workery/tenant_api/serializers/task_operation/follow_up_pending.py
@@ -132,7 +132,7 @@ class FollowUpPendingTaskOperationSerializer(serializers.Serializer):
             type_of = None
             if state == ACTIVITY_SHEET_ITEM_STATE.ACCEPTED:
                 title = _('48 hour follow up')
-                description = _('Please call up the client and confirm that the associate and client have agreed on scheduled meeting date in the future.')
+                description = _('Please call the Associate and confirm that they have scheduled a meeting date with the client.')
                 due_date = get_todays_date_plus_days(2)
                 type_of = FOLLOW_UP_IS_JOB_COMPLETE_TASK_ITEM_TYPE_OF_ID
             elif state == ACTIVITY_SHEET_ITEM_STATE.PENDING:
@@ -252,7 +252,7 @@ class FollowUpPendingTaskOperationSerializer(serializers.Serializer):
             type_of = None
             if state == ACTIVITY_SHEET_ITEM_STATE.ACCEPTED:
                 title = _('48 hour follow up')
-                description = _('Please call up the client and confirm that the associate and client have agreed on scheduled meeting date in the future.')
+                description = _('Please call the Associate and confirm that they have scheduled meeting a date with the client.')
                 due_date = get_todays_date_plus_days(2)
                 type_of = FOLLOW_UP_IS_JOB_COMPLETE_TASK_ITEM_TYPE_OF_ID
             elif state == ACTIVITY_SHEET_ITEM_STATE.PENDING:
