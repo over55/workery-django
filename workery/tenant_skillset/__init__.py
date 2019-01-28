3f44dc03cd283e1bc5efec55e22d06e29141fc49
bartmika <bart@mikasoftware.com>
2019-01-28 14:36:35 -0500

    Removed files not being used.

---
+++ workery/tenant_skillset/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'tenant_skillset.apps.TenantSkillsetConfig'
--- workery/tenant_task/__init__.py
+++ workery/tenant_task/__init__.py
@@ -1,14 +1,1 @@
 default_app_config = 'tenant_task.apps.TenantTaskConfig'
-
-'''
-#TODO: UNIT
-
-tenant_task/__init__.py                                                              2      0   100%
-tenant_task/apps.py                                                                  3      0   100%
-tenant_task/management/__init__.py                                                   0      0   100%
-tenant_task/management/commands/__init__.py                                          0      0   100%
-tenant_task/tests.py                                                               103      0   100%
-tenant_task/urls.py                                                                  6      0   100%
-tenant_task/views.py                                                               172     33    81%
-                      52, 98, 132-133, 152-201, 224-225, 251-252, 340-342, 358, 370-371
-'''
