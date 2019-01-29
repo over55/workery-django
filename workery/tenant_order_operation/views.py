ae5d36ae876bde886287995507c44af51f067f16
bartmika <bart@mikasoftware.com>
2019-01-29 14:54:05 -0500

    Repaired the job reopen operation.

--- workery/tenant_order_operation/views.py
+++ workery/tenant_order_operation/views.py
@@ -92,62 +92,17 @@ class WorkOrderPostponeOperationView(LoginRequiredMixin, GroupRequiredMixin, Ret
     return_id_required = ['search', 'summary', 'list', 'task',]
 
 
-class JobRetrieveForPostponeCreateView(LoginRequiredMixin, GroupRequiredMixin, WorkeryDetailView):
-    """
-    DEPRECATED
-    """
+class WorkOrderReopenOperationView(LoginRequiredMixin, GroupRequiredMixin, ReturnIDParameterRequiredMixin, WorkeryDetailView):
     context_object_name = 'job'
     model = WorkOrder
-    template_name = 'tenant_order/retrieve/for/postpone_view.html'
+    template_name = 'tenant_order_operation/incompleted_work_order_reopen_operation.html'
     menu_id = 'jobs'
     group_required = [
         constants.EXECUTIVE_GROUP_ID,
         constants.MANAGEMENT_GROUP_ID,
         constants.FRONTLINE_GROUP_ID
     ]
-
-    def get_context_data(self, **kwargs):
-        # Get the context of this class based view.
-        modified_context = super().get_context_data(**kwargs)
-
-        # Validate the template selected.
-        template = self.kwargs['template']
-        if template not in ['search', 'summary', 'list', 'task']:
-            from django.core.exceptions import PermissionDenied
-            raise PermissionDenied(_('You entered wrong format.'))
-        modified_context['template'] = template
-
-        # Return our modified context.
-        return modified_context
-
-
-class JobRetrieveForReopeningCreateView(LoginRequiredMixin, GroupRequiredMixin, WorkeryDetailView):
-    """
-    DEPRECATED
-    """
-    context_object_name = 'job'
-    model = WorkOrder
-    template_name = 'tenant_order/retrieve/for/reopen_view.html'
-    menu_id = 'jobs'
-    group_required = [
-        constants.EXECUTIVE_GROUP_ID,
-        constants.MANAGEMENT_GROUP_ID,
-        constants.FRONTLINE_GROUP_ID
-    ]
-
-    def get_context_data(self, **kwargs):
-        # Get the context of this class based view.
-        modified_context = super().get_context_data(**kwargs)
-
-        # Validate the template selected.
-        template = self.kwargs['template']
-        if template not in ['search', 'summary', 'list', 'task']:
-            from django.core.exceptions import PermissionDenied
-            raise PermissionDenied(_('You entered wrong format.'))
-        modified_context['template'] = template
-
-        # Return our modified context.
-        return modified_context
+    return_id_required = ['search', 'summary', 'list', 'task',]
 
 
 class TransferWorkOrderOperationView(LoginRequiredMixin, GroupRequiredMixin, ReturnIDParameterRequiredMixin, WorkeryDetailView):
