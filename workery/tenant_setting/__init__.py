e31fb2a86707c0d035736057117acc6458e16fbe
bartmika <bart@mikasoftware.com>
2019-01-29 19:22:02 -0500

    Removed useless comments.

--- workery/tenant_setting/__init__.py
+++ workery/tenant_setting/__init__.py
@@ -1,17 +1,1 @@
 default_app_config = 'tenant_setting.apps.TenantSettingConfig'
-
-'''
-#TODO: UNIT TEST
-tenant_setting/__init__.py                                                           2      0   100%
-tenant_setting/apps.py                                                               3      0   100%
-tenant_setting/urls.py                                                               5      0   100%
-tenant_setting/views/__init__.py                                                     0      0   100%
-tenant_setting/views/awaylog_views.py                                               26      0   100%
-tenant_setting/views/blacklisted_views.py                                           16      0   100%
-tenant_setting/views/insurance_requirement_views.py                                 26      0   100%
-tenant_setting/views/launchpad_views.py                                             16      0   100%
-tenant_setting/views/order_service_fee_views.py                                     26      0   100%
-tenant_setting/views/skill_set_views.py                                             34      8    76%   33-38, 54-60, 74-80
-tenant_setting/views/tag_views.py                                                   26      0   100%
-tenant_setting/views/vehicle_type_views.py                                          26      0   100%
-'''
