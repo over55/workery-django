e4f9dbf504f48fb29880c736bcf157fd0fc8b59c
bartmika <bart@mikasoftware.com>
2019-03-31 11:32:07 -0400

    Implemented customer searching by organization name. #622

--- workery/tenant_api/filters/customer.py
+++ workery/tenant_api/filters/customer.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 import django_filters
 from phonenumber_field.modelfields import PhoneNumberField
-from tenant_foundation.models import Customer
+from django.db.models import Q
+from tenant_foundation.models import Customer, Organization
 from django.db import models
 
 
 class CustomerFilter(django_filters.FilterSet):
 
     def keyword_filtering(self, queryset, name, value):
-        print(value)
         return Customer.objects.partial_text_search(value)
 
     search = django_filters.CharFilter(method='keyword_filtering')
+
+    def organization_name_filtering(self, queryset, name, value):
+        organization_pks = Organization.objects.filter(
+            Q(name__icontains=value) |
+            Q(name__istartswith=value) |
+            Q(name__iendswith=value) |
+            Q(name__exact=value) |
+            Q(name__icontains=value)
+        ).values_list("id", flat=True)
+        return queryset.filter(organization__id__in=organization_pks)
+
+    organization_name = django_filters.CharFilter(method='organization_name_filtering')
 
     class Meta:
         model = Customer
@@ -37,7 +49,8 @@ class CustomerFilter(django_filters.FilterSet):
             # # 'last_modified_by',
             # # 'comments',
             'owner__email',
-            'telephone'
+            'telephone',
+            'organization_name'
         ]
         filter_overrides = {
             models.CharField: { # given_name
