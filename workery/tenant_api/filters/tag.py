43bd1ede2fcc427d89eb2396e27a6826323e0794
bartmika <bart@mikasoftware.com>
2019-08-03 00:18:38 -0400

    Added filter for tag API endpoint.

--- workery/tenant_api/filters/tag.py
+++ workery/tenant_api/filters/tag.py
@@ -1,2 +1,32 @@
 # -*- coding: utf-8 -*-
 import django_filters   #TODO: UNIT TEST
+# -*- coding: utf-8 -*-
+import django_filters
+from phonenumber_field.modelfields import PhoneNumberField
+from tenant_foundation.models import Tag
+from django.db import models
+
+
+class TagFilter(django_filters.FilterSet):
+    o = django_filters.OrderingFilter(
+        # tuple-mapping retains order
+        fields=(
+            ('id', 'id'),
+            ('text', 'text'),
+            ('description', 'description'),
+        ),
+
+        # # labels do not need to retain order
+        # field_labels={
+        #     'username': 'User account',
+        # }
+    )
+
+    class Meta:
+        model = Tag
+        fields = [
+            # 'id',
+            # 'title',
+            # 'is_closed',
+            # 'type_of',
+        ]
--- workery/tenant_api/views/tag.py
+++ workery/tenant_api/views/tag.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import django_filters
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.conf.urls import url, include
 from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework import filters
 from rest_framework import generics
 from rest_framework import authentication, viewsets, permissions, status
 from rest_framework.response import Response
 
 from shared_foundation.custom.drf.permissions import IsAuthenticatedAndIsActivePermission
+from tenant_api.filters.tag import TagFilter
 from tenant_api.pagination import StandardResultsSetPagination
 from tenant_api.permissions.tag import (
    CanListCreateTagPermission,
@@ -28,12 +30,20 @@ class TagListCreateAPIView(generics.ListCreateAPIView):
         IsAuthenticatedAndIsActivePermission,
         CanListCreateTagPermission
     )
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
 
     def get_queryset(self):
         """
         List
         """
+        # Fetch all the queries.
         queryset = Tag.objects.all().order_by('text')
+
+        # The following code will use the 'django-filter'
+        filter = TagFilter(self.request.GET, queryset=queryset)
+        queryset = filter.qs
+
+        # Return our filtered list.
         return queryset
 
     def post(self, request, format=None):
