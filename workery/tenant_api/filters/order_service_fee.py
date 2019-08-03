79d48267f15532f024941010cfe5aa9b090ab2a7
bartmika <bart@mikasoftware.com>
2019-08-03 15:50:31 -0400

    BUGFIX: Fixed service fees API endpoint

---
+++ workery/tenant_api/filters/order_service_fee.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import django_filters   #TODO: UNIT TEST
+# -*- coding: utf-8 -*-
+import django_filters
+from phonenumber_field.modelfields import PhoneNumberField
+from tenant_foundation.models import WorkOrderServiceFee
+from django.db import models
+
+
+class WorkOrderServiceFeeFilter(django_filters.FilterSet):
+    o = django_filters.OrderingFilter(
+        # tuple-mapping retains order
+        fields=(
+            ('id', 'id'),
+            ('title', 'title'),
+            ('description', 'description'),
+            ('percentage', 'percentage'),
+        ),
+
+        # # labels do not need to retain order
+        # field_labels={
+        #     'username': 'User account',
+        # }
+    )
+
+    class Meta:
+        model = WorkOrderServiceFee
+        fields = [
+            'created_at',
+            'created_by',
+            'created_by_id',
+            'description',
+            'id',
+            'last_modified_at',
+            'last_modified_by',
+            'last_modified_by_id',
+            'percentage',
+            'title',
+            'work_orders'
+        ]
--- workery/tenant_api/views/order_service_fee.py
+++ workery/tenant_api/views/order_service_fee.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import django_filters
 from ipware import get_client_ip
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.conf.urls import url, include
 from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework import filters
 from rest_framework import generics
 from rest_framework import authentication, viewsets, permissions, status
 from rest_framework.response import Response
 
 from shared_foundation.custom.drf.permissions import IsAuthenticatedAndIsActivePermission
+from tenant_api.filters.order_service_fee import WorkOrderServiceFeeFilter
 from tenant_api.pagination import TinyResultsSetPagination
 from tenant_api.permissions.order_service_fee import (
    CanListCreateWorkOrderServiceFeePermission,
@@ -29,12 +31,20 @@ class WorkOrderServiceFeeListCreateAPIView(generics.ListCreateAPIView):
         IsAuthenticatedAndIsActivePermission,
         CanListCreateWorkOrderServiceFeePermission
     )
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
 
     def get_queryset(self):
         """
         List
         """
-        queryset = WorkOrderServiceFee.objects.all().order_by('text')
+        # Fetch all the queries.
+        queryset = WorkOrderServiceFee.objects.all().order_by('title')
+
+        # The following code will use the 'django-filter'
+        filter = WorkOrderServiceFeeFilter(self.request.GET, queryset=queryset)
+        queryset = filter.qs
+
+        # Return our filtered list.
         return queryset
 
     def post(self, request, format=None):
