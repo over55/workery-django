e316cf2f3fbc7f79c90b52b6586b4e319246b500
bartmika <bart@mikasoftware.com>
2019-08-09 17:08:59 -0400

    BUGFIX.

--- workery/tenant_api/serializers/customer_comment.py
+++ workery/tenant_api/serializers/customer_comment.py
@@ -46,7 +46,7 @@ class CustomerCustomerListCreateSerializer(serializers.ModelSerializer):
     def setup_eager_loading(cls, queryset):
         """ Perform necessary eager loading of data. """
         queryset = queryset.prefetch_related(
-            'about', 'about', 'comment'
+            'about', 'comment'
         )
         return queryset
 
