ccab4cfafda0aadd98e7e898c28be3c90b0fe3f6
bartmika <bart@mikasoftware.com>
2019-08-16 19:40:31 -0400

    Implemented partner search page.

--- workery/tenant_api/filters/partner.py
+++ workery/tenant_api/filters/partner.py
@@ -3,6 +3,7 @@ import django_filters
 from phonenumber_field.modelfields import PhoneNumberField
 from tenant_foundation.models import Partner
 from django.db import models
+from django.db.models import Q
 
 
 class PartnerFilter(django_filters.FilterSet):
@@ -12,6 +13,28 @@ class PartnerFilter(django_filters.FilterSet):
 
     state = django_filters.NumberFilter(method='state_filtering')
 
+    def email_filtering(self, queryset, name, value):
+        # DEVELOPERS NOTE:
+        # `Django REST Framework` appears to replace the plus character ("+")
+        # with a whitespace, as a result, to fix this issue, we will replace
+        # the whitespace with the plus character for the email.
+        value = value.replace(" ", "+")
+
+        # Search inside user account OR the customer account, then return
+        # our filtered results.
+        queryset = queryset.filter(
+            Q(owner__email=value)|
+            Q(email=value)
+        )
+        return queryset
+
+    email = django_filters.CharFilter(method='email_filtering')
+
+    def telephonel_filtering(self, queryset, name, value):
+        return queryset.filter(Q(telephone=value)|Q(other_telephone=value))
+
+    telephone = django_filters.CharFilter(method='telephonel_filtering')
+
     class Meta:
         model = Partner
         fields = [
@@ -20,6 +43,8 @@ class PartnerFilter(django_filters.FilterSet):
             'middle_name',
             'last_name',
             'street_address',
+            'email',
+            'telephone',
             # 'business',
             # 'birthdate',
             # 'join_date',
