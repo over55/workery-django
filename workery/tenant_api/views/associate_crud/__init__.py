cb18990f2b4c2567c114862e3e0617f85a9037af
bartmika <bart@mikasoftware.com>
2019-08-21 11:51:20 -0400

    Implemented address update API for associates

--- workery/tenant_api/views/associate_crud/__init__.py
+++ workery/tenant_api/views/associate_crud/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from tenant_api.views.associate_crud.associate_contact_update import AssociateContactUpdateAPIView
-# from tenant_api.views.associate_crud.staff_address_update import StaffAddressUpdateAPIView
+from tenant_api.views.associate_crud.associate_address_update import AssociateAddressUpdateAPIView
 # from tenant_api.views.associate_crud.staff_account_update import StaffAccountUpdateAPIView
 # from tenant_api.views.associate_crud.staff_metrics_update import StaffMetricsUpdateAPIView
--- workery/tenant_api/views/associate_crud/associate_address_update.py
+++ workery/tenant_api/views/associate_crud/associate_address_update.py
@@ -14,88 +14,18 @@ from tenant_api.permissions.associate import (
    CanListCreateAssociatePermission,
    CanRetrieveUpdateDestroyAssociatePermission
 )
-from tenant_api.filters.associate import AssociateFilter
-from tenant_api.serializers.associate import (
-    AssociateListCreateSerializer,
-    AssociateRetrieveUpdateDestroySerializer
-)
+from tenant_api.serializers.associate import AssociateRetrieveUpdateDestroySerializer
+from tenant_api.serializers.associate_crud import AssociateAddressUpdateSerializer
 from tenant_foundation.models import Associate
 from django.db import transaction
 
 
-class AssociateListCreateAPIView(generics.ListCreateAPIView):
-    serializer_class = AssociateListCreateSerializer
-    pagination_class = TinyResultsSetPagination
-    permission_classes = (
-        permissions.IsAuthenticated,
-        IsAuthenticatedAndIsActivePermission,
-        CanListCreateAssociatePermission
-    )
-    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
-    # search_fields = ('@given_name', '@middle_name', '@last_name', '@email', 'telephone', '@wsib_number')
-
-    @transaction.atomic
-    def get_queryset(self):
-        """
-        List
-        """
-        # Fetch all the queries.
-        queryset = Associate.objects.all().order_by('last_name').prefetch_related(
-            'owner',
-            # 'created_by',
-            # 'last_modified_by',
-            'tags',
-            'skill_sets',
-            # 'vehicle_types',
-            # 'comments',
-            # 'insurance_requirements',
-        )
-
-        # The following code will use the 'django-filter'
-        filter = AssociateFilter(self.request.GET, queryset=queryset)
-        queryset = filter.qs
-
-        # Return our filtered list.
-        return queryset
-
-    @transaction.atomic
-    def post(self, request, format=None):
-        """
-        Create
-        """
-        client_ip, is_routable = get_client_ip(self.request)
-        serializer = AssociateListCreateSerializer(data=request.data, context={
-            'created_by': request.user,
-            'created_from': client_ip,
-            'created_from_is_public': is_routable,
-            'franchise': request.tenant
-        })
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class AssociateRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = AssociateRetrieveUpdateDestroySerializer
-    pagination_class = TinyResultsSetPagination
+class AssociateAddressUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (
         permissions.IsAuthenticated,
         IsAuthenticatedAndIsActivePermission,
         CanRetrieveUpdateDestroyAssociatePermission
     )
-
-    @transaction.atomic
-    def get(self, request, pk=None):
-        """
-        Retrieve
-        """
-        associate = get_object_or_404(Associate, pk=pk)
-        self.check_object_permissions(request, associate)  # Validate permissions.
-        serializer = AssociateRetrieveUpdateDestroySerializer(associate, many=False)
-        return Response(
-            data=serializer.data,
-            status=status.HTTP_200_OK
-        )
 
     @transaction.atomic
     def put(self, request, pk=None):
@@ -105,45 +35,18 @@ class AssociateRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIVie
         client_ip, is_routable = get_client_ip(self.request)
         associate = get_object_or_404(Associate, pk=pk)
         self.check_object_permissions(request, associate)  # Validate permissions.
-        serializer = AssociateRetrieveUpdateDestroySerializer(associate, data=request.data, context={
+        write_serializer = AssociateAddressUpdateSerializer(associate, data=request.data, context={
             'last_modified_by': request.user,
             'last_modified_from': client_ip,
             'last_modified_from_is_public': is_routable,
             'franchise': request.tenant
         })
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @transaction.atomic
-    def delete(self, request, pk=None):
-        """
-        Delete
-        """
-        associate = get_object_or_404(Associate, pk=pk)
-        self.check_object_permissions(request, associate)  # Validate permissions.
-        associate.delete()
-        return Response(data=[], status=status.HTTP_200_OK)
-
-
-class AssociateCreateValidationAPIView(generics.CreateAPIView):
-    """
-    API endpoint strictly used for POST creation validations of the associate
-    model before an actual POST create API call is made.
-    """
-    serializer_class = AssociateListCreateSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-        IsAuthenticatedAndIsActivePermission,
-        CanListCreateAssociatePermission
-    )
-    def post(self, request, format=None):
-        """
-        Create
-        """
-        serializer = AssociateListCreateSerializer(data=request.data, context={
-            'created_by': request.user,
+        write_serializer.is_valid(raise_exception=True)
+        associate = write_serializer.save()
+        read_serializer = AssociateRetrieveUpdateDestroySerializer(associate, many=False, context={
+            'last_modified_by': request.user,
+            'last_modified_from': client_ip,
+            'last_modified_from_is_public': is_routable,
             'franchise': request.tenant
         })
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(read_serializer.data, status=status.HTTP_200_OK)
