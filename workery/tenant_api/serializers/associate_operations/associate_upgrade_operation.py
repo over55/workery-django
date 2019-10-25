2ea7546fcca45a0d963a99318065f976c4b185b7
bartmika <bart@mikasoftware.com>
2019-10-24 20:25:12 -0400

    BUGFIX: Latest progress with adding support for upgrading associates. https://github.com/over55/workery-front/issues/263

---
+++ workery/tenant_api/serializers/associate_operations/associate_upgrade_operation.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import phonenumbers
+from datetime import datetime, timedelta
+from dateutil import tz
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.contrib.auth import authenticate
+from django.db.models import Q, Prefetch
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.utils.http import urlquote
+from rest_framework import exceptions, serializers
+from rest_framework.response import Response
+from rest_framework.validators import UniqueValidator
+
+from shared_foundation.custom.drf.fields import PhoneNumberField, GenericFileBase64File
+from shared_foundation.constants import CUSTOMER_GROUP_ID
+from shared_foundation.models import SharedUser
+from shared_foundation.utils import get_content_file_from_base64_string
+# from tenant_api.serializers.associate_comment import AssociateCommentSerializer
+from tenant_foundation.constants import *
+from tenant_foundation.models import (
+    Associate,
+    PrivateImageUpload,
+    Tag
+)
+
+
+class AssociateUpgradedOperationSerializer(serializers.ModelSerializer):
+    associate = serializers.PrimaryKeyRelatedField(many=False, queryset=Associate.objects.all(), required=True,)
+
+    organization_name = serializers.CharField(
+        required=True,
+        write_only=True,
+        validators=[],
+        allow_null=True,
+        allow_blank=True,
+    )
+    organization_type_of = serializers.IntegerField(
+        required=True,
+        write_only=True,
+        validators=[]
+    )
+
+    # Meta Information.
+    class Meta:
+        model = Associate
+        fields = (
+            'id',
+            'associate',
+            'organization_name',
+            'organization_type_of',
+        )
+
+    def create(self, validated_data):
+        """
+        Override the `create` function to add extra functinality.
+        """
+        associate = validated_data.get('associate')
+        organization_name = validated_data.get('organization_name')
+        organization_type_of = validated_data.get('organization_type_of')
+
+        # # Update the password.
+        # associate.owner.set_password(password)
+        # associate.owner.pr_access_code = ''
+        # associate.owner.save()
+        # associate.last_modified_by = self.context['created_by']
+        # associate.last_modified_from = self.context['created_from']
+        # associate.last_modified_from_is_public = self.context['created_from_is_public']
+        # associate.save()
+
+        return validated_data
