f585cfe433f0dbb34cdeeb5185aa9e70ccfa4559
bartmika <bart@mikasoftware.com>
2020-05-01 12:49:55 -0400

    Increased worker count based on the number of CPUs in server.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,6 @@
+import multiprocessing
+
 command = '/opt/django/workery-django/env/bin/gunicorn'
 pythonpath = '/opt/django/workery-django/workery'
 bind = '127.0.0.1:8001'
-workers = 3
+workers = multiprocessing.cpu_count() * 2 + 1
